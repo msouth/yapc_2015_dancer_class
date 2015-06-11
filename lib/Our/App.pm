c4b6a0e5c76d827b7b2fa30f2800f05e4ab6f4db
Michael South <msouth@gmail.com>
2015-06-11 11:20:07 -0600

    solution to problem 1
    also demo of changing layout
    
    run with plackup bin/app.psgi
    
    curl -X POST  --data "name=posted_sally" http://localhost:5000/user
    
    open http://localhost:5000/user/bob in browser
    open http://localhost:5000/user/bob?test=whatever in browser
    
    template piece was with
    open http://localhost:5000/greet/bob in browser (writes to /tmp, btw)

--- lib/Our/App.pm
+++ lib/Our/App.pm
@@ -7,4 +7,26 @@ get '/' => sub {
     template 'index';
 };
 
+get '/user/:name' => sub {
+    my $input_user = param('name');
+    my $test = param('test');
+    return "Hi, $input_user, you passed [$test]";
+};
+
+post '/user' => sub {
+    my $input_user = param('name');
+    open (my $list , '>>', '/tmp/users') or die "no luck on /tmp/users: $!";
+    print $list $input_user, "\n";
+    close $list;
+    return "I saved $input_user";
+};
+
+#get '/greet/:name/job/:position' => sub {
+#param('name');
+
+get '/greet/:name' => sub {
+    my $name = param('name');
+    template 'greet_person' => {persons_name => $name };
+};
+
 true;
